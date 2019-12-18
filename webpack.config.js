67d51532c51c5cb83c61f3a63662c2fd15bea0e2
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2019-12-18 15:00:53 +0100

    Modified webpack, added images in dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,57 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
   module.exports = {
-  entry: require.resolve('./site/js/index.js'),
+    entry: require.resolve('./site/js/index.js'),
     context: __dirname,
     mode: 'development',
     output: {
       path: path.resolve(__dirname, './site/dist'),
-      filename: 'main.js'
+      filename: 'main.[contentHash].js'
   },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:"./site/template.html"
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.scss$/,
+        use: ['style-loader','css-loader','sass-loader',]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader', }
+          {
+            loader:"file-loader",
+            options: {
+              name:"[name].[contentHash].[ext]",
+              outputPath:"imgs",
+              esModule: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+        }
+      },{
+        test:/\.(woff2|woff|otf|ttf)scss$/,
+        use:[
+          {
+            loader:"file-loader",
+            options: {
+              name:"[name].[hash].[ext]",
+              outputPath: "fonts",
+              esModule: false,
+            }
+          }
         ]
       }
     ]
   }
-
- }
+ };
