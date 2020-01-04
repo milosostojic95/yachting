764c557cbbf7473992f8a6deaaef0a63f51a0329
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-01-04 16:48:35 +0100

    Added changeActive function for nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 import '../scss/main.scss';
 
+
+function changeActive () {
+  const links = document.querySelectorAll('.navbar-link > a');
+  const location = window.location;
+
+  links.forEach(link =>{
+    if(link.getAttribute('href') == location.pathname){
+     link.classList.add('active')
+    }
+    else{
+      console.log('eror')
+    }
+  })
+}
+changeActive();
