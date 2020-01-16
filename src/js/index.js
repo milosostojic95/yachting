c3bbcd25bcaf7bd4279df3df831d83e0b4c90b5d
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-01-16 13:27:21 +0100

    modified fonts size

--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,10 @@ function changeActive () {
   const links = document.querySelectorAll('.navbar-link > a');
   const location = window.location;
 
+
   links.forEach(link =>{
     if(link.getAttribute('href') == location.pathname){
-     link.classList.add('active')
+      link.classList.add('active')
     }
     else{
       console.log('eror')
